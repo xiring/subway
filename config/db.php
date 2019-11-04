140974ea3848018a32419bfb8fcc011a157797b9
tomatopotato27 <lamatshering23@gmail.com>
2019-11-04 07:41:15 +0545

    initialization of project

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=subway',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
